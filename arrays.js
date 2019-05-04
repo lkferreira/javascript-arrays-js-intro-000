ec47e11e38e3e0836c23e278ec589264048462b3
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-04 22:40:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,9 +8,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray() {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray() {
-
+  array.push(element)
 }
