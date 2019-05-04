704167d235b63b6cd4f926e4738ba3314291ccc4
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-04 22:49:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
